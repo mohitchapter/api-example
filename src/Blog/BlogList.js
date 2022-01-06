69d49d05e8af9ca2c42a1977fa5fa1e18ecfc3d8
mohitchapter <mohit.p@chapter247.com>
2022-01-06 21:25:29 +0530

    axios api call

--- src/Blog/BlogList.js
+++ src/Blog/BlogList.js
@@ -1,27 +1,62 @@
+import axios from "axios";
+import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 
-function Blogpost() {
-  return (
-    <Row>
-      <Col sm={6} md={4}>
-        <article className="App-blog-card">
-          <img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1159990/pike-place.jpg"
-            className="post-image"
-          />
-          <div className="article-details">
-            <h4 className="post-category">Travel</h4>{" "}
-            <h3 className="post-title">10 Best Things to Do in Seattle</h3>{" "}
-            <p className="post-description">
-              Seattle is a seaport city on the west coast of the United
-              States...
-            </p>
-            <p className="post-author">By Katherine Kato</p>
-          </div>
-        </article>
-      </Col>
-    </Row>
-  );
+class Blogpost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      title: "",
+      body: "",
+      data: [],
+    };
+  }
+  
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        let newData = res.data.slice(0, 10);
+        this.setState({
+          id: newData[newData.length - 1].id + 1,
+          data: newData,
+        });
+      })
+      .catch((err) => console.log("Couldn't fetch data. Error: " + err));
+  }
+
+  render() {
+    return (
+      <Row className="gy-4">
+        {this.state.data.length === 0 ? (
+          <p>Loading Posts...</p>
+        ) : (
+          this.state.data.map((post, index) => (
+            <Col sm={6} md={4} className="d-flex flex-column">
+              <article className="App-blog-card d-flex flex-column">
+                <div className="post-img-wrap position-relative">
+                <img
+                  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1159990/pike-place.jpg"
+                  className="post-image"
+                />
+                </div>
+                <div className="article-details d-flex flex-column">
+                  <h4 className="post-category">{post.userId}</h4>
+                  <h3 className="post-title">{post.title}</h3>
+                  <p className="post-description">
+                    {post.body.substr(0, 100)}...
+                  </p>
+                  <p className="post-author">By Katherine Kato</p>
+                </div>
+              </article>
+            </Col>
+          ))
+        )}
+      </Row>
+    );
+  }
 }
 
 export default Blogpost;
